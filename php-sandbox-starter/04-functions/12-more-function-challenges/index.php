58db1aed93403a443d5ba0789057949eb496a712
brandaochico <brandao.chico24@gmail.com>
2025-03-31 09:19:45 -0300

    Functions challenges.

--- php-sandbox-starter/04-functions/12-more-function-challenges/index.php
+++ php-sandbox-starter/04-functions/12-more-function-challenges/index.php
@@ -6,6 +6,22 @@
   The formula to convert Fahrenheit to Celsius is: Celsius = (Fahrenheit - 32) * 5/9
 */
 
+echo '<h2>Fahrenheit to Celsius</h2>';
+
+define('F_CONST', 32);
+// Easy
+function fahrenheitToCelsius($temp) {
+  return $temp . '°F = ' . number_format((($temp - F_CONST) * 5/9), 2) . '°C';
+} 
+
+echo 'Easier: ' . fahrenheitToCelsius(69);
+
+echo '<br>';
+// Harder
+$conversion = fn ($temp) => $temp . '°F = ' . number_format((($temp - F_CONST) * 5/9), 2) . '°C';
+
+echo 'Harder: ' . $conversion(69);
+
 echo '<br>';
 
 /*
@@ -13,6 +29,17 @@ echo '<br>';
   Create a function called `printNamesToUpperCase` that takes an array of names as an argument. The function should loop through the array and print each name to the screen in uppercase letters.
 */
 
+echo '<h2>Names to Uppercase</h2>';
+
+$names = ['Chico', 'Julia', 'Josias', 'Marcelo', 'Leandro', 'Roberta'];
+
+function printNamestoUpperCase($names) {
+  foreach($names as $name) {
+    echo strtoupper($name) . '<br>';
+  }
+}
+
+echo printNamestoUpperCase($names);
 
 echo '<br>';
 
@@ -22,3 +49,21 @@ echo '<br>';
   2. The function should return the longest word in the sentence.
   3. The output should look like this:
 */
+
+echo '<h2>Longest word in sentence</h2>';
+
+$sentence = "My name is Chico and I like to code! I'm currently learning PHP, which translates to Hypertext Processor!";
+
+function findLongestWord($sentence) {
+  $longestWord = '';
+  $words = explode(' ', $sentence);
+  foreach($words as $word) {
+    if(strlen($word) > strlen($longestWord)) {
+      $longestWord = $word;
+    }
+  }
+
+  return $longestWord;
+}
+
+echo 'The longest word in the sentence is: ' . findLongestWord($sentence);
